f380ae0d3e2e1444ea2463de0c99c5ade0c9bb65
Brav68e <thomas.dannequi@gmail.com>
2024-11-25 14:56:01 +0100

    Read() : Increased readability on submenu.py

--- submenu.py
+++ submenu.py
@@ -17,7 +17,8 @@ class MainMenu():
         else:
             self.fenetre = tk.Tk()
 
-        #Begin general settings
+
+        #General settings
         self.fenetre.title("CROWN CONQUEST")
         self.fenetre.geometry("600x600")
         self.fenetre.resizable(False, False)
@@ -45,8 +46,8 @@ class MainMenu():
         self.user1 = tk.Entry(self.fenetre, width=20)
         self.user2 = tk.Entry(self.fenetre, width=20)
         
-        title = tk.Label(self.fenetre, text="game", font=("Fast Drag Demo", 30, "bold"), fg="red")
-        title.place(x=215,y=60)
+        title = tk.Label(self.fenetre, text="CROWN CONQUEST", font=("Fast Drag Demo", 30, "bold"), fg="red")
+        title.place(x=115,y=60)
 
         choix = tk.Label(self.fenetre, text="Choisissez la taille du plateau :", font=("Arial", 15))
         choix.place(x=170, y=130)
@@ -54,13 +55,13 @@ class MainMenu():
         self.cpt=tk.StringVar()
         self.cpt.set('6')
         lbl=tk.Label(self.fenetre, width='10', textvariable=self.cpt, font='Arial 20 bold')
-        lbl.place(x=215, y=170)
+        lbl.place(x=210, y=170)
 
         '''Buttons to change the size of the board'''
         btn_plus=tk.Button(self.fenetre, text="+", command=self.plus, width='3', height='1')
-        btn_plus.place(x=320, y=225)
+        btn_plus.place(x=320, y=175)
         btn_moins=tk.Button(self.fenetre, text="-", command=self.moins, width='3', height='1')
-        btn_moins.place(x=250, y=225)
+        btn_moins.place(x=250, y=175)   #Used to be y=225
 
 
         user1_text = tk.Label(self.fenetre, text="Player 1 :", font=("Arial"))
@@ -87,47 +88,63 @@ class MainMenu():
 
         self.fenetre.mainloop()
 
+###################################################################################
+
     def entry_focus_in(self,event):
         if self.user1.get() == "Enter username here":
             self.user1.delete(0, "end")
             self.user1.config(fg="black")
 
+###################################################################################
 
     def entry_focus_out(self,event):
         if self.user1.get() == "":
             self.user1.insert(0, "Enter username here")
             self.user1.config(fg="grey")
 
+###################################################################################
+
     def entry_focus_in2(self,event):
         if self.user2.get() == "Enter username here":
             self.user2.delete(0, "end")
             self.user2.config(fg="black")
+
+###################################################################################
 
     def entry_focus_out2(self,event):
         if self.user2.get() == "":
             self.user2.insert(0, "Enter username here")
             self.user2.config(fg="grey")        
 
+###################################################################################
+
     def plus(self):
         if int(self.cpt.get()) < 12:
             self.cpt.set(int(self.cpt.get()) + 2)
 
+###################################################################################
+
     def moins(self):
         if int(self.cpt.get()) > 6:
             self.cpt.set(int(self.cpt.get()) - 2)
-        
+
+###################################################################################
+      
     def getUsername1(self):
         if self.user1.get() == "Enter username here" or "":
             return "Player 1"
         else:
             return self.user1.get()
-    
+        
+###################################################################################
+
     def getUsername2(self):
         if self.user2.get() == "Enter username here" or "":
             return "Player 2"
         else:
             return self.user2.get()
 
+###################################################################################
 
     def load_game(self):
         '''Use to select a file, read it and launch the game with those parameters'''
@@ -135,7 +152,11 @@ class MainMenu():
         self.controller.launch_game(self.fenetre, path = file_path)
 
 
+###################################################################################
+
+
     def play(self):
+        '''Function to start the game, bind with the play button'''
         p1 = self.getUsername1()
         p2 = self.getUsername2()
         #Launch the game
